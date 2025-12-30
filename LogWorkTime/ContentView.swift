48b2df9da9131cf389375d9dd239a3786bbffb80
Kurt De Jonghe <kurt.de.jonghe2gmail.com>
2025-12-30 05:21:14 +0100

    Initial Commit

---
+++ LogWorkTime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LogWorkTime
+//
+//  Created by Kurt De Jonghe on 30/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LogWorkTime/LogWorkTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogWorkTimeApp.swift
+//  LogWorkTime
+//
+//  Created by Kurt De Jonghe on 30/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct LogWorkTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
